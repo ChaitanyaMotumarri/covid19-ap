9c40f8a2d2c53f64a3f45ac330001a849ce04f88
Krishna Chaitanya <chaitanyamotumarri@gmail.com>
2020-05-09 08:46:18 +0530

    added

---
+++ app.js
@@ -0,0 +1,27 @@
+$('#btn').click(function(){
+    $.ajax({
+        method:'GET',
+        url:'https://api.covid19india.org/v2/state_district_wise.json',
+        dataType:'JSON',
+        success:function(data){
+           let districts=data[1].districtData;
+               displayData(districts);
+        }
+    })
+})
+
+let displayData=function(districts){
+    $('#count').text(districts.length);
+    let tableBody=$('#tbody');
+    let tableRow='';
+    for(let district of districts){
+        tableRow+=`<tr>
+                      <td>${district.district}</td>
+                      <td>${district.confirmed}</td>
+                      <td>${district.active}</td>
+                      <td>${district.recovered}</td>
+                      <td>${district.deceased}</td>
+                    </tr>`
+    }
+    tableBody.empty().append(tableRow);
+}
